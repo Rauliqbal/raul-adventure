f01e147f07ad98be488bc4806bc8852b633cca86
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2025-01-14 20:21:37 +0700

    build: add scene HUD

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
+@onready var coin_sfx = $"../../CoinSfx"
+
 func _on_body_entered(body: Node2D) -> void:
-	print("Ambil Coind")
+	print("Ambil Coin")
+	
+	
 	if body is PlayerController:
 		# Ambil Coin
+		coin_sfx.play()
 		GameManager.add_coin()
-		
 		queue_free() 
